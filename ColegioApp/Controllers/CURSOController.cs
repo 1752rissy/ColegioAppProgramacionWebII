e8bc0f4195398a95e9141f5dc79b3b8a044993aa
Andrés Gutierrez <andres.gutierrez@externos-cl.cencosud.com>
2022-10-08 12:39:52 -0300

    Agregar archivos de proyecto.

---
+++ ColegioApp/Controllers/CURSOController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ColegioApp.Models;
+
+namespace ColegioApp.Controllers
+{
+    public class CURSOController : Controller
+    {
+        private ColegioEntities db = new ColegioEntities();
+
+        // GET: CURSO
+        public ActionResult Index()
+        {
+            return View(db.CURSO.ToList());
+        }
+
+        // GET: CURSO/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CURSO cURSO = db.CURSO.Find(id);
+            if (cURSO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cURSO);
+        }
+
+        // GET: CURSO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CURSO/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CodigoCurso,NombreCurso")] CURSO cURSO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CURSO.Add(cURSO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cURSO);
+        }
+
+        // GET: CURSO/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CURSO cURSO = db.CURSO.Find(id);
+            if (cURSO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cURSO);
+        }
+
+        // POST: CURSO/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CodigoCurso,NombreCurso")] CURSO Curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Curso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Curso);
+        }
+
+        // GET: CURSO/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CURSO cURSO = db.CURSO.Find(id);
+            if (cURSO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cURSO);
+        }
+
+        // POST: CURSO/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CURSO cURSO = db.CURSO.Find(id);
+            db.CURSO.Remove(cURSO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ColegioApp/Controllers/ESTUDIANTEController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ColegioApp.Models;
+
+namespace ColegioApp.Controllers
+{
+    public class ESTUDIANTEController : Controller
+    {
+        private ColegioEntities db = new ColegioEntities();
+
+        // GET: ESTUDIANTE
+        public ActionResult Index()
+        {
+            var eSTUDIANTE = db.ESTUDIANTE.Include(e => e.CURSO);
+            return View(eSTUDIANTE.ToList());
+        }
+
+        // GET: ESTUDIANTE/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            if (eSTUDIANTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTUDIANTE);
+        }
+
+        // GET: ESTUDIANTE/Create
+        public ActionResult Create()
+        {
+            ViewBag.CodigoCurso = new SelectList(db.CURSO, "CodigoCurso", "NombreCurso");
+            return View();
+        }
+
+        // POST: ESTUDIANTE/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Dni,Nombre,Apellido,Telefono,CodigoCurso")] ESTUDIANTE eSTUDIANTE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ESTUDIANTE.Add(eSTUDIANTE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CodigoCurso = new SelectList(db.CURSO, "CodigoCurso", "NombreCurso", eSTUDIANTE.CodigoCurso);
+            return View(eSTUDIANTE);
+        }
+
+        // GET: ESTUDIANTE/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            if (eSTUDIANTE == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CodigoCurso = new SelectList(db.CURSO, "CodigoCurso", "NombreCurso", eSTUDIANTE.CodigoCurso);
+            return View(eSTUDIANTE);
+        }
+
+        // POST: ESTUDIANTE/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Dni,Nombre,Apellido,Telefono,CodigoCurso")] ESTUDIANTE eSTUDIANTE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eSTUDIANTE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CodigoCurso = new SelectList(db.CURSO, "CodigoCurso", "NombreCurso", eSTUDIANTE.CodigoCurso);
+            return View(eSTUDIANTE);
+        }
+
+        // GET: ESTUDIANTE/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            if (eSTUDIANTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTUDIANTE);
+        }
+
+        // POST: ESTUDIANTE/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            db.ESTUDIANTE.Remove(eSTUDIANTE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
